b537f26792294f3ae47c3473456b271ad22a95c6
florimondmanca <florimond.manca@gmail.com>
2019-06-27 00:24:24 +0200

    Update README, refactor template reading

--- tartiflette_starlette/datastructures.py
+++ tartiflette_starlette/datastructures.py
@@ -1,16 +1,13 @@
 import os
 import string
 
-from .utils import load_string_template
-
-CURDIR = os.path.dirname(__file__)
+_GRAPHIQL_TEMPLATE = os.path.join(os.path.dirname(__file__), "graphiql.html")
 
 
 class GraphiQL:
-    def __init__(self, path: str = None, template: string.Template = None):
+    def __init__(self, *, path: str = None, template: str = None):
         if template is None:
-            template = load_string_template(
-                os.path.join(CURDIR, "graphiql.html")
-            )
+            with open(_GRAPHIQL_TEMPLATE, encoding="utf-8") as f:
+                template = f.read()
         self.path = path
-        self.template = template
+        self.template = string.Template(template)
