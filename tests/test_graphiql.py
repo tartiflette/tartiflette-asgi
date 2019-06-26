34339de8a63acce24eb3dc66d27dfcbf7ae7dabe
florimondmanca <florimond.manca@gmail.com>
2019-06-26 23:46:38 +0200

    Test custom graphiql path

--- tests/test_graphiql.py
+++ tests/test_graphiql.py
@@ -1,18 +1,46 @@
+import typing
+
+import pytest
 from starlette.testclient import TestClient
 from tartiflette import Engine
 
-from tartiflette_starlette import TartifletteApp
+from tartiflette_starlette import GraphiQL, TartifletteApp
 
 
-def test_graphiql_get(client: TestClient):
-    response = client.get("/", headers={"accept": "text/html"})
-    assert response.status_code == 200
-    assert "<!DOCTYPE html>" in response.text
+@pytest.fixture(
+    name="graphiql", params=[False, True, GraphiQL(), GraphiQL(path="/graphql")]
+)
+def fixture_graphiql(request) -> typing.Union[GraphiQL, bool]:
+    return request.param
 
 
-def test_graphiql_not_found(engine: Engine):
-    ttftt = TartifletteApp(engine=engine, graphiql=False)
+@pytest.fixture(name="client")
+def fixture_client(graphiql, engine: Engine) -> TestClient:
+    ttftt = TartifletteApp(engine=engine, graphiql=graphiql)
     client = TestClient(ttftt)
-    response = client.get("/", headers={"accept": "text/html"})
+    client.headers.update({"accept": "text/html"})
+    return client
+
+
+@pytest.fixture(name="path")
+def fixture_path(graphiql) -> str:
+    if not graphiql or graphiql is True or graphiql.path is None:
+        return ""
+    return graphiql.path
+
+
+def test_graphiql(client: TestClient, graphiql, path):
+    response = client.get(path)
+
+    assert response.status_code == 200 if graphiql else 404
+
+    if response.status_code == 200:
+        assert "<!DOCTYPE html>" in response.text
+    else:
+        assert response.text == "Not Found"
+
+
+def test_graphiql_not_found(client: TestClient, path):
+    response = client.get(path + "foo")
     assert response.status_code == 404
     assert response.text == "Not Found"
