dda48b6f3ae7de45a42634e7d9714dc4825bef42
joe <joelit@me.com>
2019-10-07 20:17:33 -0500

    tartiflette upper bound to 1.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["starlette>=0.12,<0.13", "tartiflette>=0.12,<0.13"],
+    install_requires=["starlette>=0.12,<0.13", "tartiflette>=0.12,<1.2"],
     extras_require={
         "dev": [
             "uvicorn>=0.7, <0.9",
