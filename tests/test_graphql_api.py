0a7dc3c258b1d9ceb68defcdd00cd255538800ad
joe <joelit@me.com>
2019-10-25 13:48:51 -0500

    my bad -- didn't black it

--- tests/test_graphql_api.py
+++ tests/test_graphql_api.py
@@ -29,7 +29,9 @@ def test_post_json(client: TestClient):
 
 @pytest.mark.filterwarnings("ignore::DeprecationWarning")
 def test_post_invalid_json(client: TestClient):
-    response = client.post("/", data="\{test}", headers={"content-type": "application/json"})
+    response = client.post(
+        "/", data="\{test}", headers={"content-type": "application/json"}
+    )
     assert response.status_code == 400
     assert response.json() == {"error": "Invalid JSON."}
 
