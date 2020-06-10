33fd4ea75aa6352e7df1e94e3170234556fb7448
Florimond Manca <florimond.manca@gmail.com>
2020-06-10 22:24:01 +0200

    Release 0.9.0 (#121)

--- tartiflette_asgi/__init__.py
+++ tartiflette_asgi/__init__.py
@@ -1,5 +1,5 @@
 from ._app import TartifletteApp
 from ._datastructures import GraphiQL, Subscriptions
 
-__version__ = "0.8.0"
+__version__ = "0.9.0"
 __all__ = ["GraphiQL", "Subscriptions", "TartifletteApp"]
